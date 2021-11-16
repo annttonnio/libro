8bb66a81c22715fbfade60161a0c651dd6ee0d4a
annttonnio <antonio56.lop@gmail.com>
2021-11-16 11:56:57 +0100

    1847.07 libros de texto o no

--- Libro.java
+++ Libro.java
@@ -15,17 +15,20 @@ public class Libro {
     private int numeroPaginas;
     private String numeroReferencia;
     private int vecesPrestado;
+    private boolean esLibroDeTexto;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro, int numeroDePaginas)
+    public Libro(String autorLibro, String tituloLibro, int numeroDePaginas, 
+        boolean esLibroDeTextoONo)
     {
         autor = autorLibro;
         titulo = tituloLibro;
         numeroPaginas = numeroDePaginas;
         numeroReferencia = "";
         vecesPrestado = 0;
+        esLibroDeTexto = esLibroDeTextoONo;
     }
     
     public String getAutor () {
@@ -49,26 +52,42 @@ public class Libro {
     }
     
     public void imprimeDetalles(){
+        String libText;
         if (numeroReferencia.length() < 3  ){
             numeroReferencia = " zzz ";  
+        }
+        if (esLibroDeTexto == true){
+            libText = " Es un libro de texto. ";
+        }
+        else{
+            libText = " No es un libro de texto. ";
         }
         System.out.println( " El titulo del libro seleccionado es " + titulo + ". "       +"\n"+ 
                             " El autor del libro seleccionado es " + autor + ". "          +"\n"+ 
                             " El libro seleccionado tiene " + numeroPaginas + " páginas. "  +"\n"+
                             " El numero de referencia de este libro es " + numeroReferencia + ". "+"\n"+
-                            " El libro ha sido prestado "+vecesPrestado+ " veces ." );
+                            " El libro ha sido prestado "+vecesPrestado+ " veces . "+"\n"+
+                             libText);
     }
     
     public String getDetalles(){
         String varDetalles;
+        String libText;
         if (numeroReferencia.length() < 3  ){
             numeroReferencia = " zzz ";
+        }
+        if (esLibroDeTexto == true){
+            libText = " Es un libro de texto. ";
+        }
+        else{
+            libText = " No es un libro de texto. ";
         }
         varDetalles =   " El titulo del libro seleccionado es " + titulo + 
                         ". El autor del libro seleccionado es " + autor + 
                         ". El libro seleccionado tiene " + numeroPaginas + " páginas. " +
                         " El numero de referencia de este libro es " + numeroReferencia+ " . "+
-                        " El libro ha sido prestado "+ vecesPrestado + " veces .";
+                        " El libro ha sido prestado "+ vecesPrestado + " veces . "
+                        + libText;
         return varDetalles;
     }
     
@@ -90,5 +109,9 @@ public class Libro {
     
     public int getVecesPrestado(){
         return vecesPrestado;
+    }
+    
+    public boolean esLibroDeTexto(){
+        return esLibroDeTexto;
     }
 }
